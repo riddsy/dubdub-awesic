d58671f957809f578c58fdfa9161d4066b7bc839
peterids-mfto <pridlington@live.co.uk>
2021-02-19 06:14:45 +0000

    bimp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Button from './Components/Controls/Button/Button';
+import Toggle from './Components/Controls/Toggle/Toggle';
+import Label from './Components/UI/Label/Label';
 
 function App() {
   return (
     <div>
       <Button label='hey' />
+      <Toggle />
+      <Label text='yoi' />
     </div>
   );
 }
