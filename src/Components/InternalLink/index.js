b8b93aa2f0a026bc25780ac72af4a9b777a970db
peterids-mfto <pridlington@live.co.uk>
2021-04-12 09:59:42 +0100

    link

--- src/Components/InternalLink/index.js
+++ src/Components/InternalLink/index.js
@@ -21,6 +21,7 @@ export const StyledButton = styled((props) => <Link {...props} />)`
   outline: none;
   border: none;
   text-transform: uppercase;
+  text-decoration: none;
   white-space: nowrap;
   box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23);
   -webkit-box-shadow: 0 10px 20px rgba(0, 0, 0, 0.19),
